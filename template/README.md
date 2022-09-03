93c325842aa634f3943b47839dbe02571bf7e1c1
Chase <c.ruskin@ufl.edu>
2022-09-02 21:25:35 -0400

    adds basic template

---
+++ template/README.md
@@ -0,0 +1,8 @@
+# `{{ orbit.ip.library }}.{{ orbit.ip.name }}`
+
+## Overview
+
+## Navigating the project
+- `rtl`: synthesizable design code
+- `sim`: simulation code (testbenches)
+- `docs`: specification and documentation files
---
+++ template/extra/minimal.vhd
@@ -0,0 +1,28 @@
+--------------------------------------------------------------------------------
+--! Project  : {{ orbit.ip }}
+--! Engineer : {{ orbit.user }}
+--! Entity   : {{ orbit.filename }}
+--! Created  : {{ orbit.date }}
+--! Details  :
+--!     @todo: write general overview of component and its behavior
+--!
+--------------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity {{ orbit.filename }} is 
+
+    -- @todo: define port interface
+
+end entity {{ orbit.filename }};
+
+
+architecture rtl of {{ orbit.filename }} is
+
+    -- @todo: define internal signals/components
+
+begin
+
+    -- @todo: describe the circuit
+
+end architecture rtl;
